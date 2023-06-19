3f72594e496eeecd458e658e443793a7b20be327
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-19 22:59:20 +0430

    Update routes.rb recreated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :posts
-  end
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
