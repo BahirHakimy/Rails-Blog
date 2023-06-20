30202b531f816f66670542fe9518ec60e4cde48e
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-21 00:15:14 +0430

    Add views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
+require_relative '../models/user'
+
 class UsersController < ApplicationController
+  def index
+    puts 'Hello'
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
