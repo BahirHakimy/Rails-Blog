f622d25ecdbabc192da879579625a4457c304d25
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-23 15:50:28 +0430

    Add comments and post creation urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
+  post '/users/:user_id/posts', to: 'posts#create', as: 'create_post'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get '/users', to: 'users#index', as: 'users'
+  get '/users/:user_id/posts/:id/comments/new', to: 'comments#new', as: 'new_comment'
+  post '/users/:user_id/posts/:id/comments', to: 'comments#create', as: 'create_comment'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:id', to: 'users#show', as: 'user'
+  get '/users', to: 'users#index', as: 'users'
 end
