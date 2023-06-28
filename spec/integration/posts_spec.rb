9f659389d89c4eef0eb4391d350554461780d716
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-29 00:00:18 +0430

    Linters issue

--- spec/integration/posts_spec.rb
+++ spec/integration/posts_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
-
-  describe 'User post index page' do 
-    let (:user) {User.create(name: 'Tango', photo: 'http://myphoto')}
-    let (:first_post) {Post.create(author:user,title:'blablabla',text:'blablabla')}
-    let (:second_post) {Post.create(author:user,title:'dewdew',text:'dewdewdew')}
-    let (:comment) {Comment.create(user:user,post:first_post,text:"Nice one")}
-    let (:like) {Like.create(user:,post:first_post)}
-    before(:each) do 
+  describe 'User post index page' do
+    let(:user) { User.create(name: 'Tango', photo: 'http://myphoto') }
+    let(:first_post) { Post.create(author: user, title: 'blablabla', text: 'blablabla') }
+    let(:second_post) { Post.create(author: user, title: 'dewdew', text: 'dewdewdew') }
+    let(:comment) { Comment.create(user:, post: first_post, text: 'Nice one') }
+    let(:like) { Like.create(user:, post: first_post) }
+    before(:each) do
       comment.save
       like.update_likes_counter
     end
 
     it 'shows the basic info for the user' do
       visit user_posts_path(user_id: user.id)
-      
+
       # I can see the user's username.
       expect(page).to have_content(user.name)
       # I can see the number of posts the user has written.
@@ -27,12 +26,12 @@ RSpec.describe 'Posts', type: :system do
       # I can see the first comments on a post.
       expect(page).to have_content(comment.text)
       # I can see how many comments a post has.
-      expect(page).to have_css('h3',text:"Comments: #{first_post.comments.count}")
+      expect(page).to have_css('h3', text: "Comments: #{first_post.comments.count}")
       # I can see how many likes a post has.
-      expect(page).to have_css('span.post-footer',text:"Comments: #{first_post.comments_counter}, Likes: #{first_post.likes_counter}")
+      expect(page).to have_css('span.post-footer',
+                               text: "Comments: #{first_post.comments_counter}, Likes: #{first_post.likes_counter}")
       # I can see a section for pagination if there are more posts than fit on the view.
       expect(page).to have_button('Pagination')
-      
     end
 
     it "redirects me to that post's show page When I click on a post." do
@@ -43,16 +42,16 @@ RSpec.describe 'Posts', type: :system do
   end
 
   describe 'Post show page' do
-    let (:user) {User.create(name: 'Tango', photo: 'http://myphoto')}
-    let (:first_post) {Post.create(author:user,title:'blablabla',text:'blablabla')}
-    let (:second_post) {Post.create(author:user,title:'dewdew',text:'dewdewdew')}
-    let (:comment) {Comment.create(user:user,post:first_post,text:"Nice one")}
-    let (:like) {Like.create(user:,post:first_post)}
-    before(:each) do 
+    let(:user) { User.create(name: 'Tango', photo: 'http://myphoto') }
+    let(:first_post) { Post.create(author: user, title: 'blablabla', text: 'blablabla') }
+    let(:second_post) { Post.create(author: user, title: 'dewdew', text: 'dewdewdew') }
+    let(:comment) { Comment.create(user:, post: first_post, text: 'Nice one') }
+    let(:like) { Like.create(user:, post: first_post) }
+    before(:each) do
       comment.save
       like.update_likes_counter
     end
-  
+
     it 'shows the post info' do
       visit user_post_path(user_id: user.id, id: first_post.id)
       # I can see the post's title.
@@ -60,9 +59,10 @@ RSpec.describe 'Posts', type: :system do
       # I can see who wrote the post.
       expect(page).to have_content(first_post.author.name)
       # I can see how many comments it has.
-      expect(page).to have_css('h3',text:"Comments: #{first_post.comments.count}")
+      expect(page).to have_css('h3', text: "Comments: #{first_post.comments.count}")
       # I can see how many likes it has.
-      expect(page).to have_css('span.post-header',text:"Comments: #{first_post.comments_counter}, Likes: #{first_post.likes_counter}")
+      expect(page).to have_css('span.post-header',
+                               text: "Comments: #{first_post.comments_counter}, Likes: #{first_post.likes_counter}")
       # I can see the post body.
       expect(page).to have_content(first_post.text)
       # I can see the username of each commentor.
@@ -73,8 +73,6 @@ RSpec.describe 'Posts', type: :system do
       first_post.comments.each do |cm|
         expect(page).to have_content(cm.text)
       end
-      
     end
   end
-
 end
