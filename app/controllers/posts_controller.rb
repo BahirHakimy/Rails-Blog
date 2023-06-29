7be10c75365f630c6ba2bb467d75e81dc8846e79
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-29 22:57:52 +0430

    Linters issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative '../models/user'
 require_relative '../models/post'
 class PostsController < ApplicationController
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   root to: 'users#index'
   devise_for :users, path: 'auth',
