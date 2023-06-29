7096cf772f2337aab5a864f10083709d3d482871
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-06-29 21:52:31 +0430

    Added authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
   has_many :liked_posts, through: :likes, source: :post
 
-  validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
